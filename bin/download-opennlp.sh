35bc19dac40f1922cef4304d981b189c57826a68
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2021-03-07 18:00:48 -0800

    Add download script for opennlp models.

---
+++ bin/download-opennlp.sh
@@ -0,0 +1,3 @@
+curl -L http://opennlp.sourceforge.net/models-1.5/en-sent.bin -o models/opennlp/en-sent.bin
+curl -L http://opennlp.sourceforge.net/models-1.5/en-token.bin -o models/opennlp/en-token.bin
+curl -L http://opennlp.sourceforge.net/models-1.5/en-pos-maxent.bin -o models/opennlp/en-pos-maxent.bin
